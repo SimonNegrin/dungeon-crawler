185a68a2cf05b0d3abdd9e43451102b56e4a809e
Simón Negrín <simon.negrin@gmail.com>
2026-03-27 17:25:36 +0000

    Load sound effects using fetch

--- src/lib/audio.ts
+++ src/lib/audio.ts
@@ -2,7 +2,7 @@ type AudioOptions = {
   volume?: number
 }
 
-const preloaded = new Set<string>()
+const preloaded = new Map<string, Promise<string>>()
 let stepRight = true
 
 export const zipSound = createAudioPreset("zip", { volume: 0.2 })
@@ -12,7 +12,7 @@ export const tiredSound = createAudioPreset("tired", { volume: 0.4 })
 export const nextSound = createAudioPreset("next", { volume: 0.25 })
 export const stepLeftSound = createAudioPreset("step_left", { volume: 0.15 })
 export const stepRightSound = createAudioPreset("step_right", { volume: 0.15 })
-export const fogClearSound = createAudioPreset("disapear", { volume: 0.2 })
+export const fogClearSound = createAudioPreset("reveal", { volume: 0.15 })
 export const penClickSound = createAudioPreset("pen_click", { volume: 0.15 })
 export const chestOpenSound = createAudioPreset("chest_open", { volume: 0.25 })
 export const chestCloseSound = createAudioPreset("chest_close", {
@@ -32,32 +32,43 @@ function preloadAudio(audioName: string): void {
   if (preloaded.has(audioName)) {
     return
   }
-  preloaded.add(audioName)
-  const link = document.createElement("link")
-  link.rel = "preload"
-  link.as = "fetch"
-  link.href = getSrc(audioName)
-  document.head.appendChild(link)
+  preloaded.set(audioName, fetchAudio(audioName))
 }
 
-function playAudio(audioName: string, options?: AudioOptions): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const audio = document.createElement("audio")
-    audio.preload = "auto"
-    audio.src = getSrc(audioName)
-    document.body.append(audio)
-    audio.onended = () => {
-      audio.remove()
-      resolve()
-    }
-    audio.onerror = reject
+async function fetchAudio(audioName: string): Promise<string> {
+  const response = await fetch(getSrc(audioName))
+  if (!response.ok) {
+    throw new Error(`Error fetching audio with name "${audioName}"`)
+  }
+  const blob = await response.blob()
+  return URL.createObjectURL(blob)
+}
 
-    if (typeof options?.volume === "number") {
-      audio.volume = options.volume
-    }
+async function playAudio(
+  audioName: string,
+  options?: AudioOptions,
+): Promise<void> {
+  const { promise, resolve, reject } = Promise.withResolvers<void>()
+  const src = preloaded.get(audioName)
+  if (!src) {
+    throw new Error(`Audio with name "${audioName}" is not preloaded`)
+  }
+  const audio = document.createElement("audio")
+  audio.preload = "auto"
+  audio.src = await src
+  document.body.append(audio)
+  audio.onended = () => {
+    audio.remove()
+    resolve()
+  }
+  audio.onerror = reject
 
-    audio.play()
-  })
+  if (typeof options?.volume === "number") {
+    audio.volume = options.volume
+  }
+
+  audio.play()
+  return promise
 }
 
 function createAudioPreset(
